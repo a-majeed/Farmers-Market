b066ab4bb3bfdcbf6d74d30aacedf0a58f325018
Azaan Majeed <azaan@ualberta.ca>
2021-07-14 14:47:15 -0600

    playing with I/O synchronous code

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 
+// Blocking, sychronous code
+const textIn = fs.readFileSync('./txt/input.txt','utf-8');
+console.log(textIn);
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log('File written!');
 
-const hello = 'Hello world';
-console.log(hello);
