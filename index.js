3764f1cadfb36955856a4fb9d9086dd9abd34ee8
Azaan Majeed <azaan@ualberta.ca>
2021-07-14 15:13:04 -0600

    asynch I/O code test

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const fs = require('fs');
 
 // Blocking, sychronous code
-const textIn = fs.readFileSync('./txt/input.txt','utf-8');
-console.log(textIn);
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File written!');
+//const textIn = fs.readFileSync('./txt/input.txt','utf-8');
+//console.log(textIn);
+//const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+//fs.writeFileSync('./txt/output.txt', textOut);
+//console.log('File written!');
 
+
+// Non-blocking, asynchronous code
+fs.readFile('./txt/start.txt','utf-8',(err, data1) => {
+    fs.readFile(`./txt/${data1}.txt`,'utf-8',(err, data2) => {
+        console.log(data2);
+    })
+})
