1c2d402f84eddcb695ec9788bcfff55e077e0dde
Azaan Majeed <azaan@ualberta.ca>
2021-08-11 21:56:39 -0600

    changing port

--- index.js
+++ index.js
@@ -73,6 +73,4 @@ const server = http.createServer((req, res) => {
     
 });
 
-/* server.listen(8000, '127.0.0.1', () =>{
-    console.log('Listening to requests on port 8000');
-}); */
+server.listen(process.env.PORT); 
