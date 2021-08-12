0f338a97e255a765e6d6f92a5ffb03561cd41d84
Azaan Majeed <azaan@ualberta.ca>
2021-08-11 19:51:39 -0600

    dynamically replacing placeholders

--- index.js
+++ index.js
@@ -23,10 +23,22 @@ const url = require('url');
 
 // SERVER
 
-// reading file synchronously 
+const replaceTemplate = (temp, product) =>{
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+};
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product`,'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
 const dataObj = JSON.parse(data);
@@ -36,8 +48,12 @@ const server = http.createServer((req, res) => {
 
     // Overview Page
     if (pathName === '/' || pathName === '/overview'){
+        res.writeHead(200, { 'Content-type': 'text/html'});
 
-        res.end('This is the OVERVIEW');
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard,el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml);
+        console.log(cardsHtml);
+        res.end(tempOverview);
     }
     // Product Page
     else if (pathName === '/product'){
